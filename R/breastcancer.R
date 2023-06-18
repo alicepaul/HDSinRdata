8d0dcc58dc34cde92b5d6d212e8455eb04e8b7a2
joannamwalsh <75405437+joannamwalsh@users.noreply.github.com>
2023-06-17 20:38:11 -0400

    edited bc documentation

--- R/breastcancer.R
+++ R/breastcancer.R
@@ -5,19 +5,20 @@
 #' @format A data frame with 569 rows and 32 variables. The first two variables are id and diagnosis, and then the mean,
 #' standard error, and "worst" or largest (mean of the three largest values) for each of ten features are reported as follows:
 #' \describe{
-#'   \item{id}{dbl ID number}
-#'   \item{diagnosis}{dbl Diagnosis (M = malignant, B = benign)}
-#'   \item{radius}{dbl mean of distances from center to points on the perimeter}
-#'   \item{texture}{dbl standard deviation of gray-scale values}
-#'   \item{perimeter}{dbl perimeter}
-#'   \item{area}{dbl area}
-#'   \item{smoothness}{dbl local variation in radius lengths}
-#'   \item{compactness}{dbl perimeter^2 / area - 1.0}
-#'   \item{concavity}{dbl severity of concave portions of the contour}
-#'   \item{concave points}{dbl number of concave portions of the contour}
-#'   \item{symmetry}{dbl symmetry}
-#'   \item{fractal dimension}{dbl "coastline approximation" - 1}
+#'   \item{id}{ID number}
+#'   \item{diagnosis}{Diagnosis (M = malignant, B = benign)}
+#'   \item{radius}{Mean of distances from center to points on the perimeter}
+#'   \item{texture}{Standard deviation of gray-scale values}
+#'   \item{perimeter}{Perimeter}
+#'   \item{area}{Area}
+#'   \item{smoothness}{Local variation in radius lengths}
+#'   \item{compactness}{Perimeter^2 / area - 1.0}
+#'   \item{concavity}{Severity of concave portions of the contour}
+#'   \item{concave points}{Number of concave portions of the contour}
+#'   \item{symmetry}{Symmetry}
+#'   \item{fractal dimension}{"Coastline approximation" - 1}
 #' }
+#'
 #' All feature values are recoded with four significant digits.
 #'
 #' @source \url{https://archive.ics.uci.edu/dataset/15/breast+cancer+wisconsin+original}
