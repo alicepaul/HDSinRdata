82d46233104209ac633c09b781783d6113b2d828
joannamwalsh <75405437+joannamwalsh@users.noreply.github.com>
2023-06-19 16:45:08 -0400

    fixed up pain data cleaning

--- data-raw/PainCleaning.R
+++ data-raw/PainCleaning.R
@@ -2,12 +2,18 @@ library(readxl)
 
 ##### Pain Data #####
 
-#downloaded from https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0254862#sec029 (S3 Dataset)
-pain <- read.csv("journal.pone.0254862.s007.csv")
+#downloaded from https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0254862#sec029 (S1 Dataset)
+pain <- read_excel("journal.pone.0254862.s005.sheet1.xlsx")
+names(pain)[2:75] <- paste("X", names(pain)[2:75], sep = "")
+names(pain)[90:163] <- paste("X", gsub("\\s*\\([^\\)]+\\)", "", names(pain)[90:163]), ".follow_up.", sep = "")
+names(pain)[165:175] <- paste(gsub("\\s*\\([^\\)]+\\)", "", names(pain)[165:175]), ".follow_up.", sep = "")
+names(pain)[182] <- "RECODE.of.FULLBODY_CLUSTER..FULLBODY_CLUSTER."
+
 pain <- pain %>% select(-c(IMPRESSION_PAINCENTERIMPACT,
                            IMPRESSION_TREATMENTIMPACT,
                            X101.follow_up.:X238.follow_up.,
-                           race_cat, PDtotal,
+                           race_cat,
+                           PDtotal,
                            RECODE.of.FULLBODY_CLUSTER..FULLBODY_CLUSTER.,
                            PAIN_CHANGE_30percent,
                            PHYSFUNC_CHANGE_prepost,
@@ -16,8 +22,9 @@ pain <- pain %>% select(-c(IMPRESSION_PAINCENTERIMPACT,
                            RESP_HIGH,
                            lnBODYREGIONSUM,
                            lnBODYREGIONSUMfollowup,
-                           PROMIS_PHYSICAL_FUNCTION.follow_up.:IMPRESSION_PAINCENTERIMPACT.follow.up.,
-                           BODYREGIONSUM.followup.,
+                           PROMIS_PHYSICAL_FUNCTION.follow_up.:IMPRESSION_PAINCENTERIMPACT.follow_up.,
+                           BODYREGIONSUM.follow_up.,
                            PAIN_CHANGE_prepost,
                            PAIN_CHANGE_fraction))
+
 usethis::use_data(pain, overwrite = TRUE)
