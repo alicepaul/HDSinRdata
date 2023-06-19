e3a4a64a60eccef2f7a6f1acdd871f74a158206f
joannamwalsh <75405437+joannamwalsh@users.noreply.github.com>
2023-06-19 18:48:52 -0400

    pain edits

--- data-raw/PainCleaning.R
+++ data-raw/PainCleaning.R
@@ -5,11 +5,13 @@ library(readxl)
 
 #downloaded from https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0254862#sec029 (S1 Dataset)
 pain <- read_excel("journal.pone.0254862.s005.sheet1.xlsx")
+names(pain)
 names(pain)[2:75] <- paste("X", names(pain)[2:75], sep = "")
 names(pain)[90:163] <- paste("X", gsub("\\s*\\([^\\)]+\\)", "", names(pain)[90:163]), ".follow_up.", sep = "")
 names(pain)[c(165:173, 175)] <- paste(gsub("\\s*\\([^\\)]+\\)", "", names(pain)[c(165:173, 175)]), ".follow_up.", sep = "")
 names(pain)[182] <- "RECODE.of.FULLBODY_CLUSTER..FULLBODY_CLUSTER."
-names(pain)[88] <- "PAIN_INTENSITY_AVERAGE.follow_up"
+names(pain)[164] <- "PAIN_INTENSITY_AVERAGE.follow_up"
+
 pain <- pain %>% select(-c(IMPRESSION_PAINCENTERIMPACT,
                            IMPRESSION_TREATMENTIMPACT,
                            X101.follow_up.:X238.follow_up.,
