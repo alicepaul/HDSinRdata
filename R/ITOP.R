b465ec5878dc3c2ff8eb3de0eb13556e1691c104
joannamwalsh <75405437+joannamwalsh@users.noreply.github.com>
2023-06-29 18:35:06 -0400

    documented ITOP data

---
+++ R/ITOP.R
@@ -0,0 +1,31 @@
+#' Statistics on Induced Terminations of Pregnancy (ITOP) in Texas
+#'
+#' Texas abortion counts by race/ethnicity and county of residence from 2016
+#' to 2021 from the Texas Department of State Health Services (up to June 2018)
+#' and the Health and Human Services Commission since then
+#'
+#' @format A data frame with 1,368 rows and 10 variables:
+#' \describe{
+#'   \item{County}{County of residence in Texas}
+#'   \item{Total}{Total number of abortions}
+#'   \item{Asian}{Total number of abortions among Asian women}
+#'   \item{Hispanic}{Total number of abortions among Hispanic women}
+#'   \item{White}{Total number of abortions among White women}
+#'   \item{Black}{Total number of abortions among Black women}
+#'   \item{Native American}{Total number of abortions among Native American
+#'   women}
+#'   \item{Other}{Total number of abortions among women of other races or
+#'   ethnicities}
+#'   \item{Not Stated}{Total number of abortions among women who did not state
+#'   their race or ethnicity}
+#'   \item{Year}{Year}
+#' }
+#'
+#' Note from the data website: for the year 2020, "Data do not include 82
+#' reports submitted after statutory deadlines and that were not available when
+#' annual data were compiled."
+#'
+#' @source Data was obtained Texas Health and Human Services ISTOP Statistics
+#'
+#' \url{https://www.hhs.texas.gov/about/records-statistics/data-statistics/itop-statistics}
+"ITOP"
